edd705a67ee328390349a519da734316f24981d9
shiqi.cai <shiqi.cai@supernovacompanies.biz>
2022-02-18 15:36:30 +0800

    fix

--- src/main/java/com/supernovacompanies/api/controller/ApiController.java
+++ src/main/java/com/supernovacompanies/api/controller/ApiController.java
@@ -5,16 +5,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 @Api(value = "open api")
 @Slf4j
 public class ApiController {
 
-    @GetMapping("/startApplication")
+    @GetMapping("/v5/{universe}/startApplication")
     @ApiOperation(value = "test api")
     public ApiResponse<String> startApplication() {
         log.info("[startApplication] api start");
